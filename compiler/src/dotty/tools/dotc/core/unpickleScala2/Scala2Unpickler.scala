d71247b381ed922c5d9b9e1dee049506569c1ee0
Georg Schmid <georg.schmid@epfl.ch>
2018-01-23 19:52:27 +0100

    Avoided relying on class symbols of primitive value types during unpickling. They might not be initialized yet (as seen during bootstrapped tests).

--- compiler/src/dotty/tools/dotc/core/unpickleScala2/Scala2Unpickler.scala
+++ compiler/src/dotty/tools/dotc/core/unpickleScala2/Scala2Unpickler.scala
@@ -426,8 +426,7 @@ class Scala2Unpickler(bytes: Array[Byte], classRoot: ClassDenotation, moduleClas
     // TODO(gsps): This is a hack to mark certain primitive methods as stable.
     //  In the future such primitive methods should be pickled with the Stable flag set.
     def markPrimitiveStable(owner: Symbol, name: Name, flags: FlagSet): FlagSet =
-      if (defn.ScalaValueClasses().contains(owner)) flags | Stable
-      else flags
+      if (tpnme.ScalaValueNames.contains(defn.scalaClassName(owner))) flags | Stable else flags
 
     tag match {
       case NONEsym => return NoSymbol
